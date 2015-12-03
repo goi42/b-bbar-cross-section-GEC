b610b892090e38de519143146817ae079202ed15
Michael Wilkinson <miwilkin@syr.edu>
2015-12-03 16:52:20 +0100

    Test is optsfile with [File()] syntax; Reco is inputdata

--- teststripping/DV_v37r2p4/test/makedst/GangaJob.py
+++ teststripping/DV_v37r2p4/test/makedst/GangaJob.py
@@ -6,7 +6,6 @@ myApplication.user_release_area = '~/cmtuser'
 j = Job(name='noGEC_dst', application=myApplication, backend=Dirac())
 j.comment = 'all LFN, commented out input in optsfile, listed Reco as optsfile'
 j.splitter = SplitByFiles(filesPerJob=5)
-myOptsfiles = ['./TestMyStrippingLine_noGEC_modified.py','./Reco15a_Run164668_modified.py']
-j.application.optsfile = myOptsfiles
-j.inputdata = j.application.readInputData(myOptsfiles)
+j.application.optsfile = [File('./TestMyStrippingLine_noGEC_modified.py')]
+j.inputdata = j.application.readInputData('./Reco15a_Run164668_modified.py')
 j.submit()
