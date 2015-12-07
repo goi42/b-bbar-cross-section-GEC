3bf0dc97bb4aea0a295fd24b47630a8c8281b9e5
Michael Wilkinson <miwilkin@syr.edu>
2015-12-07 17:03:23 +0100

    copied Max's stuff to work with

---
+++ teststripping/DV_v37r2p4/test/makedst/trymoreLFNs/submitGanga.py
@@ -0,0 +1,14 @@
+j = Job(application=DaVinci(version='v38r0'))
+j.backend = Dirac()
+j.name = 'noGEC'
+j.inputdata = j.application.readInputData('Reco15a_Run164668_modified.py')
+j.inputdata.depth=2
+j.application.optsfile = 'TestMyStrippingLineAndCreateNtuple.py'
+
+j.outputfiles = [LocalFile("DVTuplesTestMy.root")]
+j.splitter = SplitByFiles(filesPerJob = 10,maxFiles = -1)
+j.do_auto_resubmit = True
+
+
+j.submit()
+#queues.add(j.submit)
