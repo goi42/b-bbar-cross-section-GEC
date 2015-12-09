c68a283b0afd07e64553a543bcd8f1bb5256fe94
Michael Wilkinson <miwilkin@syr.edu>
2015-12-09 18:42:58 +0100

    moved looping statements to proper indentation

--- teststripping/DV_v37r2p4/test/makedst/try2015LFNs/GangaOutput.py
+++ teststripping/DV_v37r2p4/test/makedst/try2015LFNs/GangaOutput.py
@@ -6,7 +6,7 @@ for sj in j.subjobs:
    if sj.status != 'completed': continue
    for f in sj.outputfiles.get(DiracFile):
       #if (sj.id == 21 or sj.id == 57 or sj.id == 61 or sj.id == 48 or sj.id ==386):
-         base_path = '/tmp/mwilkins/'
-         os.makedirs(os.path.join(base_path, sj.fqid))
-         f.localDir = os.path.join(base_path, sj.fqid)
-         f.get()       
+      base_path = '/tmp/mwilkins/'
+      os.makedirs(os.path.join(base_path, sj.fqid))
+      f.localDir = os.path.join(base_path, sj.fqid)
+      f.get()       
