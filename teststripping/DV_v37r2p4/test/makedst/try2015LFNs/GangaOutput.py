fd703a8f7c8eb44b92e4423a2c5f95c46d12822c
Michael Wilkinson <miwilkin@syr.edu>
2015-12-09 19:04:38 +0100

    added line to make sure directories are only created if they don't exist, allowing multiple files to be placed in the same directory without error. Also changed so that the jid can be specified when this is called. Also made some minor syntax changes to make it (hopefully) more efficient and readable.

--- teststripping/DV_v37r2p4/test/makedst/try2015LFNs/GangaOutput.py
+++ teststripping/DV_v37r2p4/test/makedst/try2015LFNs/GangaOutput.py
@@ -1,12 +1,14 @@
 import os, sys
-#jid = sys.argv[1]
-j = jobs(300)
+jid = sys.argv[1]
+j = jobs(jid)
+base_path = '/tmp/mwilkins/'
 
 for sj in j.subjobs:
    if sj.status != 'completed': continue
    for f in sj.outputfiles.get(DiracFile):
       #if (sj.id == 21 or sj.id == 57 or sj.id == 61 or sj.id == 48 or sj.id ==386):
-      base_path = '/tmp/mwilkins/'
-      os.makedirs(os.path.join(base_path, sj.fqid))
-      f.localDir = os.path.join(base_path, sj.fqid)
-      f.get()       
+      whole_path = os.path.join(base_path, sj.fqid)
+      if not os.path.exists(whole_path):
+         os.makedirs(whole_path)
+      f.localDir = whole_path
+      f.get()
