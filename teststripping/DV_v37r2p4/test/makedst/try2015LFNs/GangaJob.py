bbcc776a0bd711f9d1841be9325c5c3b149cc97a
Michael Wilkinson <miwilkin@syr.edu>
2015-12-09 15:56:38 +0100

    uncommented like 50 LFNs and changed GangaJob to read data from LHCb*

--- teststripping/DV_v37r2p4/test/makedst/try2015LFNs/GangaJob.py
+++ teststripping/DV_v37r2p4/test/makedst/try2015LFNs/GangaJob.py
@@ -8,27 +8,27 @@ myApplication.version = 'v37r2p4'
 myApplication.optsfile = [File('./TestMyStrippingLineAndCreateNtuple.py')]
 
 j = Job(name='noGEC_2015DST', application=myApplication, backend=Dirac())
-j.comment = 'uses a BK query to pick up '
+j.comment = 'job 298 but with a local list of LFNs'
 j.splitter = SplitByFiles(filesPerJob=5)
 j.do_auto_resubmit = True
 j.outputfiles = [DiracFile('*.root')] #has to match what's in opts file
 
-BK_locations = [
-    'LHCb/Collision15/Beam6500GeV-VeloClosed-MagDown/Real Data/Reco15a/Stripping23r1/90000000/SEMILEPTONIC.DST',
-    'LHCb/Collision15/Beam6500GeV-VeloClosed-MagUp/Real Data/Reco15a/Stripping23r1/90000000/SEMILEPTONIC.DST'
-    ]
-data = LHCbDataset()
-bk = BKQuery()
-for path in BK_locations:
-    bk.path = path
-    tmp = bk.getDataset()
-    if len(tmp.files) > 0:
-        data.extend( tmp )
+# BK_locations = [
+#     'LHCb/Collision15/Beam6500GeV-VeloClosed-MagDown/Real Data/Reco15a/Stripping23r1/90000000/SEMILEPTONIC.DST',
+#     'LHCb/Collision15/Beam6500GeV-VeloClosed-MagUp/Real Data/Reco15a/Stripping23r1/90000000/SEMILEPTONIC.DST'
+#     ]
+# data = LHCbDataset()
+# bk = BKQuery()
+# for path in BK_locations:
+#     bk.path = path
+#     tmp = bk.getDataset()
+#     if len(tmp.files) > 0:
+#         data.extend( tmp )
 
-import sys
-if len(data.files) < 1:
-    sys.exit()
+# import sys
+# if len(data.files) < 1:
+#     sys.exit()
 
-j.inputdata = data
+j.inputdata = j.application.readInputData('./LHCb_Collision15_Beam6500GeVVeloClosedMagUp_Real Data_Reco15a_Stripping23r1_90000000_SEMILEPTONIC.DST.py')
 
 j.submit()
